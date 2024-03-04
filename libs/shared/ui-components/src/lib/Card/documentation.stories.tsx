528133596e888b486a9be5c4486841eaab530277
cyerena <cyerena@integralads.com>
2024-03-04 10:48:33 -0600

    Add Card documentation

---
+++ libs/shared/ui-components/src/lib/Card/documentation.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta } from '@storybook/react';
+import { Card } from '.';
+
+const meta: Meta<typeof Card> = {
+  component: Card,
+  title: 'Card',
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+export const FullExample = () => {
+  return (
+    <Card>
+      <Card.Header>
+        <h1 style={{ paddingLeft: 24, margin: 0 }}>Card Header</h1>
+      </Card.Header>
+      <Card.Body>
+        <span>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro
+          doloremque velit, quidem magnam asperiores dolorem voluptates totam id
+          ipsum adipisci, itaque tempora explicabo officia esse magni. Voluptas,
+          repellendus? Sapiente, molestias!
+        </span>
+      </Card.Body>
+    </Card>
+  );
+};
