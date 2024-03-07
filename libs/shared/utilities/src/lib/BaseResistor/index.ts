1bb8a41577951f61143ddc741ab23189053d4433
cyerena <cyerena@integralads.com>
2024-03-06 21:38:22 -0600

    Add BaseResistor class

---
+++ libs/shared/utilities/src/lib/BaseResistor/index.ts
@@ -0,0 +1,32 @@
+type ResistorConfig = {
+  type: string;
+  values: {
+    [key: string]: string;
+  };
+};
+
+export class BaseResistor {
+  config: ResistorConfig[];
+
+  constructor(config: ResistorConfig[]) {
+    this.config = config;
+  }
+
+  calculateValues(bandColors: string) {
+    const resistorValues: { [key: string]: string } = {};
+
+    this.config.forEach(({ type, values }, idx) => {
+      const currentBandColor = bandColors[idx];
+
+      if (resistorValues[type]) {
+        resistorValues[
+          type
+        ] = `${resistorValues[type]}${values[currentBandColor]}`;
+      } else {
+        resistorValues[type] = values[currentBandColor];
+      }
+    });
+
+    return resistorValues;
+  }
+}
