49ad5d613f857724965b48770fef19314c834b3d
cyerena <cyerena@integralads.com>
2024-03-10 20:45:44 -0600

    Update constants library

--- libs/shared/utilities/src/lib/constants/types.ts
+++
@@ -1,7 +0,0 @@
-import { REGULAR_BAND_COLORS } from './BandColors';
-
-export type ColorCode<T extends readonly string[]> = {
-  [K in T[number]]: number;
-};
-
-export type BandValue = (typeof REGULAR_BAND_COLORS)[number];
