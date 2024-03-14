ffa6e7c68b4a591c2924f5256d9431deb7554047
Jaime Yerena <63207315+CharleyDvs@users.noreply.github.com>
2024-03-14 17:45:24 -0600

    Setup scripts for e2e testing (#18)
    Co-authored-by: cyerena <cyerena@integralads.com>

--- apps/resistor-calculator-e2e/src/e2e/app.cy.ts
+++ apps/resistor-calculator-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
-
 describe('resistor-calculator-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should load components correctly', () => {
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    cy.get('h1').should('be.visible');
+    cy.get('button').contains('bands resistor');
+    cy.get('h2').contains('Resistor Values');
   });
 });
